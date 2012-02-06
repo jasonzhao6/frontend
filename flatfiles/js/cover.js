73ab18234c81cfb0f35d36ed36b40825d5746d61
Jason Zhao <jasonzhao6@gmail.com>
2012-02-06 03:00:58 -0800

    added flatfiles, bootstrap2, and index.html

---
+++ flatfiles/js/cover.js
@@ -0,0 +1,40 @@
+/*! Cover.js | Full screen background support for IE8 and below */
+(function($) {
+  
+  var $image, imageSize, imageAspectRatio, windowSize, windowAspectRatio,
+      initialize = function() {
+        $("html").css("background-image", "none");
+        $image = $("#background-cover");
+        $image.css("max-width", "none");
+        $image.show();
+        imageSize = {
+          width: $image.data("width"),
+          height: $image.data("height")
+        },
+        imageAspectRatio = imageSize.width / imageSize.height;
+        $(window).resize(resizeAction).trigger('resize');
+  		},
+  		resizeAction = function() {
+  			windowSize = {
+  				height	: $(window).height(),
+  				width	: $(window).width()
+  			},
+  			windowAspectRatio = windowSize.width / windowSize.height;
+  			if (imageAspectRatio > windowAspectRatio) {
+          $image.css('left', Math.min(((windowSize.width - imageSize.width * windowSize.height / imageSize.height) / 2), 0));
+          $image.css('top', 0);
+          $image.width('auto');
+          $image.height('100%');
+  			} else {
+          $image.css('top', Math.min(((windowSize.height - imageSize.height * windowSize.width / imageSize.width) / 2), 0));
+          $image.css('left', 0);
+          $image.width('100%');
+          $image.height('auto');
+  			}
+  		};
+  
+  if ($('html').css('background-size') !== 'cover') {
+    $(initialize);
+  }
+  
+})(jQuery);
